039e8a3a8a130132474ba6fc3b8e7353bc7a27b3
KienCT5 <kienct.work@gmail.com>
2022-12-30 14:12:22 +0700

    End of code base

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities { get; set; }
+    }
+}
