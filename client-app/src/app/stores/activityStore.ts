60802a3593e29b16f31ca821e87c4a92737d793c
KienCT5 <kingofdespair69@gmail.com>
2023-01-04 01:39:12 +0700

    add and using mobX to client

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,113 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
+import { v4 as uuid } from 'uuid';
+
+export default class ActivityStore {
+    activitiesRegistry = new Map<string, Activity>();
+    selectedActivity: Activity | undefined = undefined;
+    editMode = false;
+    loading = false;
+    loadingInitial = true;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    get activitiesByDate() {
+        return Array.from(this.activitiesRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date)
+        );
+    }
+
+    loadActivities = async () => {
+        //this.setLoadingInitial(true);
+        try {
+            const activities = await agent.Activities.list();
+
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activitiesRegistry.set(activity.id, activity);
+            })
+            this.setLoadingInitial(false);
+        } catch (error) {
+            console.log(error);
+            this.setLoadingInitial(false);
+        }
+    }
+
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
+
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activitiesRegistry.get(id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }
+
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuid();
+        try {
+            await agent.Activities.create(activity);
+            runInAction(() => {
+                this.activitiesRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                this.activitiesRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    deleteActivity = async (id: string) => {
+        this.loading = true;
+        try {
+            await agent.Activities.delete(id);
+            runInAction(() => {
+                this.activitiesRegistry.delete(id);
+                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+}
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +13,10 @@ root.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>\
-  <App />
-  
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
