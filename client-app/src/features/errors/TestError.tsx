cc119778af8755c16b1ea2ed99e07722ed306fa9
kienct <80740282+wibu009@users.noreply.github.com>
2023-03-13 23:55:40 +0700

    update buggy url

--- client-app/src/features/errors/TestError.tsx
+++ client-app/src/features/errors/TestError.tsx
@@ -4,31 +4,30 @@ import axios from 'axios';
 import ValidationError from './ValidationError';
 
 export default function TestErrors() {
-    const baseUrl = process.env.REACT_APP_API_URL;
     const [errors, setErrors] = useState(null);
 
     function handleNotFound() {
-        axios.get(baseUrl + 'buggy/not-found').catch(err => console.log(err.response));
+        axios.get('/buggy/not-found').catch(err => console.log(err.response));
     }
 
     function handleBadRequest() {
-        axios.get(baseUrl + 'buggy/bad-request').catch(err => console.log(err.response));
+        axios.get('/buggy/bad-request').catch(err => console.log(err.response));
     }
 
     function handleServerError() {
-        axios.get(baseUrl + 'buggy/server-error').catch(err => console.log(err.response));
+        axios.get('/buggy/server-error').catch(err => console.log(err.response));
     }
 
     function handleUnauthorised() {
-        axios.get(baseUrl + 'buggy/unauthorised').catch(err => console.log(err.response));
+        axios.get('/buggy/unauthorised').catch(err => console.log(err.response));
     }
 
     function handleBadGuid() {
-        axios.get(baseUrl + 'activities/notaguid').catch(err => console.log(err.response));
+        axios.get('/activities/notaguid').catch(err => console.log(err.response));
     }
 
     function handleValidationError() {
-        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err));
+        axios.post('/activities', {}).catch(err => setErrors(err));
     }
 
     return (
