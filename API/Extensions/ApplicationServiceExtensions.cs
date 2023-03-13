0706bd824e76995d58540e4b61c80bd21e82e80d
kienct <80740282+wibu009@users.noreply.github.com>
2023-03-14 02:05:07 +0700

    fix DI in API

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -47,42 +47,39 @@ namespace API.Extensions
             services.AddDbContext<DataContext>(opt =>
             {
                 //opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-                services.AddDbContext<DataContext>(options =>
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                //Depending on if in development or production, use either FlyIO
+                //connection string, or development connection string from env var.
+                if (env == "Development")
                 {
-                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    // Use connection string from file.
+                    connStr = config.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    // Use connection string provided at runtime by FlyIO.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                    string connStr;
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
 
-                    //Depending on if in development or production, use either FlyIO
-                    //connection string, or development connection string from env var.
-                    if (env == "Development")
-                    {
-                        // Use connection string from file.
-                        connStr = config.GetConnectionString("DefaultConnection");
-                    }
-                    else
-                    {
-                        // Use connection string provided at runtime by FlyIO.
-                        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                }
 
-                        // Parse connection URL to connection string for Npgsql
-                        connUrl = connUrl.Replace("postgres://", string.Empty);
-                        var pgUserPass = connUrl.Split("@")[0];
-                        var pgHostPortDb = connUrl.Split("@")[1];
-                        var pgHostPort = pgHostPortDb.Split("/")[0];
-                        var pgDb = pgHostPortDb.Split("/")[1];
-                        var pgUser = pgUserPass.Split(":")[0];
-                        var pgPass = pgUserPass.Split(":")[1];
-                        var pgHost = pgHostPort.Split(":")[0];
-                        var pgPort = pgHostPort.Split(":")[1];
-
-                        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-                    }
-
-                    // Whether the connection string came from the local development configuration file
-                    // or from the environment variable from FlyIO, use it to set up your DbContext.
-                    options.UseNpgsql(connStr);
-                });
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from FlyIO, use it to set up your DbContext.
+                opt.UseNpgsql(connStr);
             });
 
             services.AddCors(opt =>
